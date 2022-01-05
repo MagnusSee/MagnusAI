6763b8dddf908d5f6933fb874bc64b7e86be2809
MagnusSEE <lukiechess@gmail.com>
2022-01-04 20:56:42 -0500

    Release 1.0 (bit buggy)

---
+++ magnus.py
@@ -0,0 +1,13 @@
+import os
+from neuralintents import GenericAssistant
+
+magnus = GenericAssistant('intents.json')
+magnus.train_model()
+magnus.save_model()
+
+print("Magnus Alpha")
+while True:
+    msg = input('>>>')
+    print('\n')
+    responseMsg = magnus.request(msg)
+    print('Response:',responseMsg)
