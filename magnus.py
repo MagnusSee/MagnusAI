b916123af4420c3fa140da714ce12f9f2b6a2dcf
MagnusSEE <lukiechess@gmail.com>
2022-01-08 16:18:57 -0500

    Quality of life improvement

--- magnus.py
+++ magnus.py
@@ -1,14 +1,27 @@
 #Made by MagnusSEE
 import os
 from neuralintents import GenericAssistant
+import platform
 
+#Create system variable which has the name of the OS
+system = platform.system()
+
+#Initalize the AI
 magnus = GenericAssistant('intents.json')
 magnus.train_model()
 magnus.save_model()
 
+#Darwin is MacOS
+if system == 'Windows':
+    os.system('cls')
+elif system == 'Darwin' or system == 'Linux':
+    os.system('clear')
+
 print("Magnus Alpha")
+
+#Command listening loop
 while True:
-    msg = input('>>>')
+    msg = input(' >>>')
     print('\n')
     responseMsg = magnus.request(msg)
     print('Response:',responseMsg)
